39bdc5b90b54de5870249419fe8fc3523c7aec9a
Aly Mustafa <67666092+aly205@users.noreply.github.com>
2026-05-31 14:51:37 -0500

    Add: is-anagram - submission-15

---
+++ Data Structures & Algorithms/is-anagram/submission-15.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_chars = dict()
+        t_chars = dict()
+        for a in s:
+            s_chars[a] = s_chars.get(a, 0) + 1
+        for b in t:
+            t_chars[b] = t_chars.get(b, 0) + 1
+
+        return s_chars == t_chars
+
+        
